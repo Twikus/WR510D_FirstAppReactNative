771db8ffca2f4e147826a194d407f993be6e97c2
Axel D <axel@spliiit.com>
2023-12-15 09:04:45 +0100

    feat : add assets

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import * as Font from 'expo-font';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoadingScreen from './pages/Loading';
+import HomeScreen from './pages/Home';
 
 const Stack = createStackNavigator();
 
@@ -12,8 +13,12 @@ const App = () => {
   useEffect(() => {
     async function loadFont() {
       await Font.loadAsync({
-        'Montserrat-Regular': require('./assets/fonts/Montserrat-Regular.ttf'),
-        'Montserrat-Bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+        'poppins-regular': require('./assets/fonts/Poppins-Regular.ttf'),
+        'poppins-medium': require('./assets/fonts/Poppins-Medium.ttf'),
+        'poppins-bold': require('./assets/fonts/Poppins-Bold.ttf'),
+        'poppins-semi-bold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+        'poppins-light': require('./assets/fonts/Poppins-Light.ttf'),
+        'poppins-thin': require('./assets/fonts/Poppins-Thin.ttf'),
       });
       setFontLoaded(true);
     }
@@ -31,6 +36,10 @@ const App = () => {
         <Stack.Screen
           name="Loading"
           component={LoadingScreen}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
